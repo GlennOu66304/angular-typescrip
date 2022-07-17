a9f9a1b02fd0874ad161af195af2610c96656337
Glenn Ou <2738328919@qq.com>
2022-07-17 15:27:36 +0800

    display all content, a content, delete a content

---
+++ src/app/article.ts
@@ -0,0 +1,16 @@
+export class Article {
+    _id: string;
+    title: string;
+    author: string;
+    description: string;
+    content: string;
+    createdAt: string;
+  }
+
+  // What TypeScript version is Visual Studio Code using? How to update it?
+  // https://stackoverflow.com/questions/39668731/what-typescript-version-is-visual-studio-code-using-how-to-update-it
+  // Property '...' has no initializer and is not definitely assigned in the constructor
+  
+  // https://stackoverflow.com/questions/49699067/property-has-no-initializer-and-is-not-definitely-assigned-in-the-construc?page=1&tab=scoredesc#tab-top
+  // Angular TS类报错：Property ‘name‘ has no initializer and is not definitely assigned in the constructor
+  // https://blog.csdn.net/zhouyingge1104/article/details/115472095
--- src/app/show-article/show-article.component.ts
+++ src/app/show-article/show-article.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Article } from '../article';
 
 @Component({
   selector: 'app-show-article',
   templateUrl: './show-article.component.html',
-  styleUrls: ['./show-article.component.css']
+  styleUrls: ['./show-article.component.css'],
 })
 export class ShowArticleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  article: Article = {
+    _id: '',
+    title: '',
+    author: '',
+    description: '',
+    content: '',
+    createdAt: '',
+  };
+  isLoadingResults = true;
+  constructor(
+    private route: ActivatedRoute,
+    private api: ApiService,
+    private router: Router
+  ) {}
+  getArticleDetails(id: any) {
+    this.api.getArticle(id).subscribe((data: any) => {
+      this.article = data;
+      console.log(this.article);
+      this.isLoadingResults = false;
+    });
   }
 
+  deleteArticle(id: any) {
+    this.isLoadingResults = true;
+    this.api.deleteArticle(id).subscribe(
+      (res) => {
+        this.isLoadingResults = false;
+        this.router.navigate(['/articles']);
+      },
+      (err) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      }
+    );
+  }
+  ngOnInit(): void {
+    this.getArticleDetails(this.route.snapshot.params['id']);
+    // this.deleteArticle(this.route.snapshot.params['id']);
+  }
 }
