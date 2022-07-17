63211d8868421af2a214045213c9f704a2df9d04
Glenn Ou <2738328919@qq.com>
2022-07-17 11:17:33 +0800

    router and page build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ShowArticleComponent } from './show-article/show-article.component';
+import { AddArticleComponent } from './add-article/add-article.component';
+import { EditArticleComponent } from './edit-article/edit-article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticlesComponent,
+    ShowArticleComponent,
+    AddArticleComponent,
+    EditArticleComponent
   ],
   imports: [
     BrowserModule,
