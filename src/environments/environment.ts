ff2ca54d96999594577e7476e3c7680369b88ed6
Glenn Ou <2738328919@qq.com>
2023-03-07 00:02:42 +0400

    apiUrlChange
    apiUrlChange

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://nest-typescript-mongodb.herokuapp.com'
+  apiURL: 'https://nest-typescript-mongodb-api2.onrender.com'
 };
 
 /*
